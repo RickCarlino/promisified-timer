346a0a753986c87f801ae50ec83d223e6783fe3c
Rick Carlino <rick.carlino@gmail.com>
2016-06-22 00:33:53 -0500

    No more UMD, actually

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-module.exports = require('./lib/pomisified-timer');
+"use strict";
+var names = {};
+exports.Timer = {};
+exports.Timer.start = function (name, delay) {
+    if (typeof name === 'string') {
+        exports.Timer.stop(name);
+        return new Promise(function (resolve, reject) {
+            names[name] = setTimeout(function () {
+                if (names[name]) {
+                    resolve(name);
+                    delete names[name];
+                }
+                else {
+                    reject(); // cleared
+                }
+            }, delay);
+        });
+    }
+    if (typeof name === 'number') {
+        delay = name;
+        return new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                resolve(name);
+            }, delay);
+        });
+    }
+    throw new Error("Timer expects a number or string.");
+};
+exports.Timer.stop = function (name) {
+    if (typeof name == 'string') {
+        if (names[name]) {
+            clearTimeout(names[name]);
+            delete names[name];
+        }
+    }
+};
--- lib/promisified-timer.d.ts
+++
@@ -1,7 +0,0 @@
-export interface PromisifiedTimer {
-    /** Start a timer with a label, allowing cancelation. */
-    start(name: string, delay: number): Promise<string>;
-    start(name: number): Promise<number>;
-    stop(name: string): any;
-}
-export declare var Timer: PromisifiedTimer;
